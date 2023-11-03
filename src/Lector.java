224bce2e16c3dcb828630a6e13d37d78ff6d9424
earbis <jabyruiz@gmail.com>
2023-11-03 14:06:52 +0100

    Update Lector.java

--- src/Lector.java
+++ src/Lector.java
@@ -1,16 +1,41 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Lector {
-	public static void main(String[] args) throws IOException {
-		
+	File fichero;
+	FileReader fr = new FileReader(fichero);
+	BufferedReader br = new BufferedReader(fr);
 	
+	public Lector() throws FileNotFoundException {
+		super();
+	}
+
+	public Lector(File fichero, FileReader fr, BufferedReader br) throws FileNotFoundException {
+		super();
+		this.fichero = fichero;
+		this.fr = fr;
+		this.br = br;
+	}
+	
+
+	public void setFichero(File fichero) {
+		this.fichero = fichero;
+	}
+
+	public static void main(String[] args) throws IOException {
+	
+		
+		
+	/*
 	//metodo para abrir el fichero
 	File fichero = new File(".\\archivoCSV.csv");
 	File resultado = new File(".\\ResultadoCSV.csv");
@@ -32,5 +57,22 @@ public class Lector {
 	//metodo para procesar la informacion
 	//metodo para escribir el resultado en otro fichero
 	
+	*/
+	}
 	
-}}
+	public void abrir(String documento) throws IOException {
+		try {
+			fichero = new File(documento);
+			
+			String c;
+
+			while ((c = br.readLine	()) != null) {
+				System.out.println(c);
+			}
+		}catch (FileNotFoundException e) { System.out.println();}	
+	}
+	public Map<String, Integer> analizar() {
+		Map<String, Integer> diccionario = new HashMap<String, Integer>();
+		return diccionario;
+	}
+}
